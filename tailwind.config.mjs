68e67ac0d7fadaaaa8e7638f101ac69dc6dfe2a2
jaydippatel83 <jaydippatel83.pj@gmail.com>
2025-02-05 16:29:55 +0530

    init

---
+++ tailwind.config.mjs
@@ -0,0 +1,30 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    extend: {
+       
+      colors: {
+        background: "var(--background)",
+        foreground: "var(--foreground)",
+        green: {
+          300: '#86efac',
+          400: '#4ade80',
+          500: '#22c55e',
+          600: '#16a34a',
+          700: '#15803d',
+        },
+        border: {
+          DEFAULT: '#4ade80', 
+          dark: '#4ade80', 
+          light: '#d1d5db', 
+        },
+      },
+    },
+  },
+  plugins: [],
+};
