342223d81ff3f56404a3a77decff1cc2c87c1c4a
mansijoshi17 <mansijoshi1777@gmail.com>
2025-02-06 11:50:46 +0530

    Added nft contract for subscription

---
+++ contracts/ScamBuzzerNFT.sol
@@ -0,0 +1,46 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.19;
+
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+contract ScamBuzzerSubscriptionNFT is ERC721URIStorage, Ownable(msg.sender) {
+    uint256 public nextTokenId;
+    uint256 public constant PRICE = 0.01 ether; // Price of the subscription NFT
+    string public baseURI;
+
+    mapping(address => bool) public hasActiveSubscription;
+    mapping(uint256 => uint256) public expirationTimestamp;
+
+    event SubscriptionPurchased(address indexed subscriber, uint256 tokenId, uint256 expiration);
+
+    constructor(string memory _baseURI) ERC721("ScamBuzzer", "SBS") {
+        baseURI = _baseURI;
+    }
+
+    function _baseURI() internal view override returns (string memory) {
+        return baseURI;
+    }
+
+    function purchaseSubscription() external payable {
+        require(msg.value >= PRICE, "Insufficient payment");
+        require(!hasActiveSubscription[msg.sender], "Already subscribed");
+
+        uint256 tokenId = nextTokenId++;
+        _mint(msg.sender, tokenId);
+        _setTokenURI(tokenId, string(abi.encodePacked(baseURI, Strings.toString(tokenId), ".json")));
+
+        expirationTimestamp[tokenId] = block.timestamp + 365 days;
+        hasActiveSubscription[msg.sender] = true;
+
+        emit SubscriptionPurchased(msg.sender, tokenId, expirationTimestamp[tokenId]);
+    }
+
+    function isSubscriptionActive(address user) external view returns (bool) {
+        return hasActiveSubscription[user] && expirationTimestamp[nextTokenId - 1] > block.timestamp;
+    }
+
+    function withdraw() external onlyOwner {
+        payable(owner()).transfer(address(this).balance);
+    }
+}
